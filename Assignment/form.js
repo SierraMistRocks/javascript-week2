31f07d87cceff6f5ffc789594d1b9bf10c596301
SierraMistRocks <158002866+SierraMistRocks@users.noreply.github.com>
2024-03-11 18:07:39 -0500

    some changes

--- Assignment/form.js
+++ Assignment/form.js
@@ -1,13 +1,16 @@
-let formHTML = document.getElementById("contactform")
-formHTML.addEventListener("submit", function(){
+
+let formHTML = document.getElementById('form')
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const formHTML = document.getElementById("form");
+    formHTML.addEventListener("submit", function (){
     Event.preventDefault()
     let randomDecimalBetween0and200 = Math.random() * 200
     let randomWholeBetween0and200 = Math.floor(randomDecimalBetween0and200)
     console.log(randomWholeBetween0and200)
 
     let subjectInputHTML = document.getElementById("subject")
-    let subjectinput = subjectInputHTML.value
-    subjectinput = subjectInputHTML.value + " #" + randomWholeBetween0and200
+    let subjectinput = subjectInputHTML.value + " #" + randomWholeBetween0and200
 
     let emailInputHTML = document.getElementById("email")
     let emailinput = emailInputHTML.value
@@ -16,8 +19,13 @@ formHTML.addEventListener("submit", function(){
     let bodyinput = bodyInputHTML
 
     window.location.href = `mailto:mariatalbert@gmail.com?subject=${subjectinput}&cc=${emailinput}&body=${bodyinput}`
-})
+});
+  });
 
-
-
-
+if (formHTML) {
+    formHTML.addEventListener('submit', () => {
+      console.log('You visited the link');
+    });
+  } else {
+    console.log('You failed to retrieve the element.')
+  }
